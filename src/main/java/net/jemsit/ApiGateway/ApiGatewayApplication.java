90fc66e1ecdf8fe21569918fb846ce0dbf55751b
jimmy <jimishukurow@gmail.com>
2024-08-28 13:39:30 +0300

    first commit

---
+++ src/main/java/net/jemsit/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package net.jemsit.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+//	public GlobalFilter authFilter() {
+//		return new AuthFilter();
+//	}
+}
---
+++ src/main/java/net/jemsit/ApiGateway/AuthFilter.java
@@ -0,0 +1,19 @@
+package net.jemsit.ApiGateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class AuthFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
